49e1c3628eaaa459caedb07772682c4b14e8237b
MilanRavenell <milanravenell@gmail.com>
2019-07-24 12:43:18 -0700

    copying index.js to master

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-const http = require('http');
+var express = require('express');
+var app = express();
+var sql = require("mssql");
+const port = process.env.PORT;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//config for your database
+var config = {
+    user: 'babyfoodadmin@babyfood',
+    password: 'babyfoodpass69!',
+    server: 'babyfood.database.windows.net',
+    connectionTimeout: 30000,
+    database: 'BabyFoodDB',
+    port: 1433,
+    options: { encrypt: true } 
+};
+
+app.post('/retrieve', function (req, res) {
+    // connect to your database
+    var ac = req.body.acronym;
+
+    new sql.ConnectionPool(config).connect().then(pool => {
+        return pool.request().query(`SELECT * FROM acronyms WHERE acronym='${ac}';`)
+        }).then(result => {
+            let rows = result.recordset
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.status(200).json(rows);
+                console.log(rows);
+            sql.close();
+        }).catch(err => {
+            res.status(500).send(err)
+            sql.close();
+    });
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
+app.post('/add', function (req, res) {
+    // connect to your database
+    var ac = req.body.acronym;
+    var desc = req.body.description;
 
-console.log("Server running at http://localhost:%d", port);
+    new sql.ConnectionPool(config).connect().then(pool => {
+        return pool.request().query(`INSERT INTO acronyms (acronym, description) VALUES ('${ac}', '${desc}');`)
+        }).then(result => {
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            res.status(200).send('Success');
+            sql.close();
+        }).catch(err => {
+            res.status(500).send(err)
+            sql.close();
+    });
+});
+
+app.listen(port, () => console.log('Server is running..'));
