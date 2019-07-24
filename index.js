274a0ebbc967dc45119358a9d0b6e8f9033f0613
Chuck Wang <charwan@microsoft.com>
2019-07-24 15:51:45 -0700

    Add update functionality

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ app.post('/retrieve', function (req, res) {
     var ac = req.body.acronym;
 
     new sql.ConnectionPool(config).connect().then(pool => {
-        return pool.request().query(`SELECT * FROM Acronyms WHERE Acronym='${ac}';`)
+        return pool.request().query(`SELECT * FROM Acronyms WHERE Acronym='${ac}' ORDER BY id DESC;`)
         }).then(result => {
             let rows = result.recordset
             res.setHeader('Access-Control-Allow-Origin', '*')
+            res.send(rows);
             res.status(200).json(rows);
                 console.log(rows);
             sql.close();
@@ -49,6 +50,28 @@ app.post('/add', function (req, res) {
         }).catch(err => {
             res.status(500).send(err)
             sql.close();
+    });
+});
+
+app.post('/update', function (req, res) {
+    // connect to your database
+    var ac = req.body.acronym;
+    var desc = req.body.description;
+    var index = req.body.index;
+
+    // ensure acronym is in the database first
+
+    new sql.ConnectionPool(config).connect().then(pool => {
+        return pool.request().query(`SELECT *, ROW_NUMBER() OVER (ORDER BY id) AS rownum FROM Acronyms WHERE Acronym='${ac}' HAVING row = ${index};`)
+    }).then(pool => {
+        return pool.request().query(`UPDATE Acronyms SET (Acronym, Description) VALUES ('${ac}', '${desc}') WHERE id = ${result.recordset[0]["id"] } ; `)
+    }).then(result => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.status(200).send('Success');
+        sql.close();
+    }).catch(err => {
+        res.status(500).send(err)
+        sql.close();
     });
 });
 
--- myJsBots/my-chat-bot/bot.js
+++ myJsBots/my-chat-bot/bot.js
@@ -9,21 +9,23 @@ class BabyBot extends ActivityHandler {
         super();
 
         this.onMessage(async (context, next) => {
-            // format is "what does xxx mean?"
+
             var message = context.activity.text.split(" ");
             console.log(message);
-            if (message.length == 4 &&
-                message[0] == "what" &&
+
+            // asking for definition
+            // format: "What does <word> mean?
+            if (message.length == 4 &&  
+                message[0] == "What" &&
                 message[1] == "does" &&
-                message[3] == "mean?")
-            {
+                message[3] == "mean?") {
 
                 var word = message[2];
 
                 const options = {
                     method: 'POST',
                     uri: 'https://babyfoodapp.azurewebsites.net/retrieve',
-                    body: { acronym: `${ word }` },
+                    body: { acronym: `${word}` },
                     json: true,
                     rejectUnauthorized: false,
                     requestCert: false,
@@ -31,28 +33,34 @@ class BabyBot extends ActivityHandler {
                 };
 
                 await rp(options)
-                    .then(async function (response) {
+                    .then(async function (response) {    //no definition
                         if (response.length == 0) {
                             await context.sendActivity(`Sorry, ${word} hasn't been added yet. You can add a description with:\n\n \"${word} means <description>\"`);
                         }
-                        else {
-                            for (var i = 0; i < response.length; i++) {
-                                await context.sendActivity(`${ response[i]["Description"] }`);
+                        else {                          //return definitions
+                            if (response.length > 1) {
+                                await context.sendActivity(`Multiple definitions were found:`);
                             }
-                        }  
+                            for (var i = 0; i < response.length; i++) {
+                                await context.sendActivity(`${response[i]["Description"]}`);
+                            }
+                        }
                         console.log(response);
                     })
                     .catch(async function (err) {
                         console.log(err);
                     });
-            } else if (message.length > 2 && message[1] == "means") {
+
+            // defining the word
+            // format: "<word> means <def>"
+            } else if (message.length > 3 && message[1] == "means") {
                 var word = message[0];
                 var description = message.slice(2, message.length);
 
                 const options = {
                     method: 'POST',
                     uri: 'https://babyfoodapp.azurewebsites.net/add',
-                    body: { acronym: `${ word }`, description: `${description}` },
+                    body: { acronym: `${word}`, description: `${description}` },
                     json: true,
                     rejectUnauthorized: false,
                     requestCert: false,
@@ -67,6 +75,44 @@ class BabyBot extends ActivityHandler {
                     .catch(async function (err) {
                         console.log(err);
                     });
+             //update word
+            // format: "Update <word> to <def>"
+            } else if (message.length > 3 && message[0] == "Update") {
+
+                ////format: "Update <word> <optional #> <>"
+                ////var j = 0; //optional #
+                ////var slicei = 2; //start of slice dependant on #
+                ////// isnumeric
+                ////if (!isNaN(parseFloat(message[2])) && isFinite(message[2])) {
+
+                ////    j = message[2];
+                ////    slicei = 3;
+                ////}
+
+                var word = message[1];
+                var description = message.slice(3, message.length);
+                var index = 1;
+
+                const options = {
+                    method: 'POST',
+                    uri: 'https://babyfoodapp.azurewebsites.net/update',
+                    body: { acronym: `${word}`, description: `${description}` , index: `${index}`},
+                    json: true,
+                    rejectUnauthorized: false,
+                    requestCert: false,
+                    agent: false
+                };
+
+                await rp(options)
+                    .then(async function (response) {
+                        await context.sendActivity(`${word} has been updated. Thanks!`);
+                        console.log(response);
+                    })
+                    .catch(async function (err) {
+                        console.log(err);
+                    });
+
+
             }
 
             await next();
