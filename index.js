1cb81cff53f242c97c84415a82b0053c79592189
MilanRavenell <milanravenell@gmail.com>
2019-07-23 18:08:40 -0700

    database works

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
   			res.status(200).json(rows);
     		sql.close();
   		}).catch(err => {
-    		res.status(500).send({ message: "${err}"})
+    		res.status(500).send({ message: "${err}""})
     		sql.close();
   	});
 
